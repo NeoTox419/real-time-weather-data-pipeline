30f180c0005ba3b8d08f98ad24b1a8d3faa80765
Saikat <saikatrock2k@gmail.com>
2026-01-12 21:24:26 +0530

    Add OpenWeather API ingestion script

---
+++ Ingestion/fetch_weather.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
+from datetime import datetime
+
+# Load environment variables
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+CITY = "Kolkata"
+URL = "https://api.openweathermap.org/data/2.5/weather"
+
+params = {
+    "q": CITY,
+    "appid": API_KEY,
+    "units": "metric"
+}
+
+def fetch_weather():
+    response = requests.get(URL, params=params)
+    response.raise_for_status()
+
+    data = response.json()
+    data["ingested_at"] = datetime.utcnow().isoformat()
+
+    return data
+
+if __name__ == "__main__":
+    weather_data = fetch_weather()
+    print(weather_data)
